3a46eb7569dccd35972a0d3a741976f636cc7375
andrefbmaximino <andrefbmaximino@gmail.com>
2021-08-12 17:44:56 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://andref:andref@cluster0-i2frd.mongodb.net/test?retryWrites=true"
+    mongoURI: "mongodb+srv://andref:andref@cluster0.i2frd.mongodb.net/test?retryWrites=true&w=majority"
 };
